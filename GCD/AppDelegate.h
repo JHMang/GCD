fb36f60f4d2c4e8e7b2ff177c125524ec71bb6fc
马锦航 <13839567147@163.com>
2017-06-04 14:44:25 +0800

    Initial Commit

---
+++ GCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD
+//
+//  Created by 马锦航 on 2017/6/4.
+//  Copyright © 2017年 黑点. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD
+//
+//  Created by 马锦航 on 2017/6/4.
+//  Copyright © 2017年 黑点. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
